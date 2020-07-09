73e85e45bd4942359c8f097e7f343a8ef3820f73
Eun Seok <jeseok95@gmail.com>
2020-07-09 14:57:42 +0900

    7/8 javax란

---
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/BaseTimeEntity.java
@@ -0,0 +1,18 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    private
+}
